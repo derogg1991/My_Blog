372dd629c805c797d394200c4c2e129d0ff8257d
root <root@anthing.com>
2018-07-14 23:27:16 -0400

    mysql支持

--- MYblog/settings.py
+++ MYblog/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'MYblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'wary',
+        'PASSWORD': '896966my))',
+        'PORT': '3306'
     }
 }
 
@@ -120,4 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
