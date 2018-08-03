95123b54c4f7452daaab79ec7519057a788c8462
root <root@cross.com>
2018-08-03 17:47:26 +0800

    基本完成

--- MYblog/settings.py
+++ MYblog/settings.py
@@ -76,21 +76,21 @@ WSGI_APPLICATION = 'MYblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	}
-}
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'blog',
-#        'USER': 'wary',
-#        'PASSWORD': '896966my))',
-#        'PORT': '3306'
-#    }
+#   'default': {
+#		'ENGINE': 'django.db.backends.sqlite3',
+#		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#	}
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '896966my))',
+        'PORT': '3306'
+    }
+}
 
 
 # Password validation
--- README.md
+++ README.md
@@ -2,7 +2,10 @@ project name = MYblog
 Python=3.5.2
 部署流程:
 1. 调整settings关于数据库的设定
-2. uwsgi启动
-3. nginx启动
+2. source venv
+3. pip install -r requirements.txt
+4. makemigrations && migrate
+5. uwsgi启动
+6. nginx启动
 
 ### 本博客源自教程:https://www.zmrenwu.com/post/2/
